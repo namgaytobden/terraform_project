41b3812a44589232745fbd28e6bfd00df31b5916
namgaytobden <121597739+namgaytobden@users.noreply.github.com>
2023-02-17 12:37:54 +0600

    Update linux_vm.tf

--- modules/linux_vm.tf
+++ modules/linux_vm.tf
@@ -1,7 +1,7 @@
 module "linuxvm" {
     source = "../terraform/linuxvm"
     resource_group_name="minirg"
-    resource_group_location="Australia East`"
+    resource_group_location="Australia East"
     virtual_network_name="vnet1"
     subnet_name="subnet1"
     network_security_group_name="nsg1"
